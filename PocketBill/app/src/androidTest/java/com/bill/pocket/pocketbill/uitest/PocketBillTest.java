e9402389ebbe979f5f4e18557c4f024b4e28251a
Thomas Niedermayr <thomas.niedermayr@student.tugraz.at>
2015-04-26 10:18:28 +0200

    Added first TestCase and made it pass + removed all warnings

---
+++ PocketBill/app/src/androidTest/java/com/bill/pocket/pocketbill/uitest/PocketBillTest.java
@@ -0,0 +1,30 @@
+package com.bill.pocket.pocketbill.uitest;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.bill.pocket.pocketbill.MainActivity;
+import com.robotium.solo.Solo;
+
+public class PocketBillTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo mySolo;
+
+    public PocketBillTest(){
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo ( getInstrumentation (), getActivity ());
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testCategoryList(){
+        mySolo.clickOnText("Gas");
+        mySolo.clickOnText("Groceries");
+        mySolo.clickOnText("Shopping");
+    }
+}
--- PocketBill/app/src/main/java/com/bill/pocket/pocketbill/MainActivity.java
+++ PocketBill/app/src/main/java/com/bill/pocket/pocketbill/MainActivity.java
@@ -1,17 +1,39 @@
 package com.bill.pocket.pocketbill;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String[] categories = new String[] {"Gas","Groceries","Shopping"};
+    final ArrayList<String> list = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView categoryView = (ListView) findViewById(R.id.CategoryView);
+
+        list.addAll(Arrays.asList(categories));
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        categoryView.setAdapter(adapter);
+
+
     }
 
 
@@ -35,5 +57,30 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
