d67fc9c33612be401c20983d6a4fa2c32ce7fec9
comeclarity <t.leh@gmx.at>
2015-05-20 11:28:49 +0200

    [TL, MR] feature branch search created

--- PocketBill/app/src/main/java/com/bill/pocket/pocketbill/MainActivity.java
+++ PocketBill/app/src/main/java/com/bill/pocket/pocketbill/MainActivity.java
@@ -256,9 +256,9 @@ public class MainActivity extends ActionBarActivity {
             }
 
             );
-
-        }
  
+        }
+
         @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
